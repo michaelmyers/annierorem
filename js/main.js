72db0d1190c45871f8ab22ba112eead5405901e1
Michael M. Myers <michaelmmyers@Michaels-MacBook-Pro.local>
2014-01-17 00:09:29 -0500

    initial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+    $.fn.fullpage({
+        slidesColor: ['#34495e', '#34495e', '#34495e'],
+        anchors: ['home', 'about', 'novum-io', 'robopogo'],//, 'dc-transit'],
+        verticalCentered: false,
+        resize: false
+    });
+});
